aed5ec262d0d83b2d4543276d1082780a52a9f77
Tuana Celik <tuana.celik@deepset.ai>
2023-09-21 20:30:24 +0200

    some minor haystack updates

--- statschat/llm.py
+++ statschat/llm.py
@@ -9,22 +9,13 @@ class Summarizer:
     to arbitrary text.
     """
 
-    default_prompt_template = PromptTemplate(
-        name="lfqa",
-        prompt_text="""Synthesize a comprehensive answer from the following text
-        for the given question. Provide a clear and concise response, that summarizes
-        the key points and information presented in the text. Your answer should be
-        in your own words and be no longer than 50 words. If the question cannot be
-        confidently answered from the information in the text, or if the question is
-        not related to the text, reply 'NA'. \n\n
-        Related text: {join(documents)} \n\n Question: {query} \n\n Answer:""",
-    )
+    default_prompt_template = PromptTemplate("./prompts/lfqa.yml")
 
     def __init__(
-        self, model_name_or_path: str = "google/flan-t5-large", prompt_text=None
+        self, model_name_or_path: str = "google/flan-t5-large", prompt_text=None,
     ):
         if prompt_text:
-            self.prompt = PromptTemplate(name="lfqa", prompt_text=prompt_text)
+            self.prompt = PromptTemplate(prompt=prompt_text)
         else:
             self.prompt = self.default_prompt_template
 
